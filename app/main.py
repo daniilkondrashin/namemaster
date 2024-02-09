7f548ecfd9dac6edcd6d31be1f7a0a52ca3d2135
Daniil <dankon368024@gmail.com>
2024-02-10 01:31:55 +0400

    delete index.html

--- app/main.py
+++ app/main.py
@@ -4,22 +4,19 @@ from collections import namedtuple
 from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
-from dotenv import load_dotenv, find_dotenv
 
 from form import Name
 
-load_dotenv(find_dotenv())
 
 app = Flask(__name__)
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.environ['postgresql-username']}:{os.environ['postgresql-password']}@{os.environ['postgresql-host']}:{os.environ['postgresql-port']}/{os.environ['postgresql-database']}"
-app.config['SECRET_KEY'] = os.getenv('S_KEY')  
+app.config['SECRET_KEY'] = {os.environ['namemaster-secretkey']}  
 db = SQLAlchemy(app)
 
 Messages = namedtuple('Messages', 'text')
 messagesSession = []
-
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,13 +29,7 @@ class Message(db.Model):
 with app.app_context():
     db.create_all()
 
-
-@app.route('/', methods=['GET'])
-def hello_world():
-    return render_template('index.html')
-
-
-@app.route("/main", methods=['POST', 'GET'])
+@app.route("/", methods=['POST', 'GET'])
 def main():
     form = Name()
     if form.validate_on_submit():
